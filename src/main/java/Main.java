f2e4cea9a908f90fa702470b23d0d892a18dfaea
werest <weresttrade@ya.ru>
2019-06-14 22:38:06 +0300

    Revert "testPiramid - System.exit(0)"
    This reverts commit 31ca816f

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,9 @@
-import nu.pattern.OpenCV;
-import org.opencv.core.Core;
+
 
 public class Main {
 
 
     public static void main(String[] args) {
-
-        OpenCV.loadLocally();
-
-        Constant cnt = new Constant();
-
-        cnt.run();
-        cnt.run(("Welcome to OpenCV " + Core.VERSION));
-
-        new MyAPI().testPiramid(new Constant().pathResources + new Constant().imgPath);
 
     }
 }
